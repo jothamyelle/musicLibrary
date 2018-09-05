d390c74bc42745fb9d2bbf7fc97f76a205e9d0bc
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-05 21:51:15 +0000

    Using classes and prototypes to create a music library

--- library.js
+++ library.js
@@ -1,109 +1,72 @@
-var library = {
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             },
-   // FUNCTIONS TO IMPLEMENT:
-  // prints a list of all playlists, in the form:
-  // p01: Coding Music - 2 tracks
-  // p02: Other Playlist - 1 tracks
-  printPlaylists : function () {
-    for (list in this.playlists) {
-        console.log(this.playlists[list].id + ": " + this.playlists[list].name + " - " + this.playlists[list].tracks.length + " tracks");
-    }
-  },
-  // prints a list of all tracks, in the form:
-  // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
-  // t02: Model View Controller by James Dempsey (WWDC 2003)
-  // t03: Four Thirty-Three by John Cage (Woodstock 1952)
-  printTracks : function () {
-    for (list in this.tracks) {
-        console.log(this.tracks[list].id + ": " + this.tracks[list].name + " by " + this.tracks[list].artist + " (" + this.tracks[list].album + ")");
-    }
-  },
-  // adds an existing track to an existing playlist
-  addTrackToPlaylist : function (trackId, playlistId) {
-      if(this.tracks.hasOwnProperty(trackId)) {
-          this.playlists[playlistId].tracks.push(trackId);
-      }
-},
-// prints a list of tracks for a given playlist, in the form:
-// p01: Coding Music - 2 tracks
-// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
-// t02: Model View Controller by James Dempsey (WWDC 2003)
-printPlaylist : function (playlistId) {
-    if(this.playlists.hasOwnProperty(playlistId)) {
-        console.log(this.playlists[playlistId].id + ": " + this.playlists[playlistId].name + " - " + this.playlists[playlistId].tracks.length + " tracks");
-        var tracksInPlaylist = [];
-        for (track in this.playlists[playlistId].tracks) {
-            tracksInPlaylist.push(this.playlists[playlistId].tracks[track]);
-        }
-        for (track in tracksInPlaylist) {
-            console.log(this.tracks[tracksInPlaylist[track]].id + ": " + this.tracks[tracksInPlaylist[track]].name + " by " + this.tracks[tracksInPlaylist[track]].artist + " (" + this.tracks[tracksInPlaylist[track]].album + ")");
-        }
-    }
-},
-// adds a track to the this
-addTrack : function (name, artist, album) {
-  var newTrack = {
-      id: uid(),
-      name: name,
-      artist: artist,
-      album: album
-  };
-  this.tracks[newTrack.id] = newTrack;
-  console.log(this.tracks);
-},
-// adds a playlist to the this
-addPlaylist : function (name) {
-  var newPlaylist = {
-      id: uid(),
-      name: name,
-      tracks: []
-  };
-  this.playlists[newPlaylist.id] = newPlaylist;
-},
-// STRETCH:
-// given a query string string, prints a list of tracks
-// where the name, artist or album contains the query string (case insensitive)
-// tip: use "string".search("tri")
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
-printSearchResults : function(query) {
-  query = query.toLowerCase();
-  for (list in this.tracks) {
-      if (this.tracks[list].name.toLowerCase().search(query) >= 0 || this.tracks[list].artist.toLowerCase().search(query) >= 0 || this.tracks[list].album.toLowerCase().search(query) >= 0) {
-         console.log(this.tracks[list].id + ": " + this.tracks[list].name + " by " + this.tracks[list].artist + " (" + this.tracks[list].album + ")");
-      }
+class Library {
+  constructor(name, creator) {
+    this.name = name;
+    this.creator = creator;
+    this.playlists = [];
+  }
+
+  addPlaylist(playlistName) {
+    let newPlaylist = new Playlist(playlistName);
+    this.playlists.push(newPlaylist);
+    return newPlaylist;
   }
 }
+
+class Playlist {
+  constructor(name) {
+    this.name = name;
+    this.tracks = [];
+  }
+
+  addTrack(title, rating, duration) {
+    let newTrack = new Track(title, rating, duration);
+    this.tracks.push(newTrack);
+  }
+
+  get overallRating() {
+    let sum = 0;
+    this.tracks.forEach(track => {
+      sum += track.rating;
+    });
+    let average = sum / tracks.length;
+    return average;
+  }
+
+  get totalDuration() {
+    let sum = 0;
+    this.tracks.forEach(track => {
+      sum += track.duration;
+    });
+    return sum;
+  }
 }
-// generates a unique id
-// (use this for addTrack and addPlaylist)
-var uid = function() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+
+class Track {
+  constructor(title, rating, duration) {
+    this.title = title;         // string
+    this.rating = rating;       // int 1-5
+    this.duration =  duration;  // int in seconds
+  }
 }
-library.printPlaylists();
-library.printTracks();
-library.printPlaylist('p01');
-library.addTrackToPlaylist('t03','p01');
-library.addTrack("Kanwal's Best Song", "Kanwal Gupta", "Kanwal's Love: Franklin");
-library.addPlaylist("Our Brand New Playlist");
-library.printSearchResults("james");
+
+var jothamsLibrary = new Library("Jotham's Music Library","Jotham");
+var jothamsCoolPlaylist = jothamsLibrary.addPlaylist("Jotham's Awesome Funky Playlist");
+jothamsCoolPlaylist.addTrack("Cool Song Bro", 5, 198);
+jothamsCoolPlaylist.addTrack("Cooler Song", 4, 135);
+jothamsCoolPlaylist.addTrack("One Song", 2, 18);
+jothamsCoolPlaylist.addTrack("Still Dre", 5, 402);
+jothamsCoolPlaylist.addTrack("Still Dre, But Twice", 3, 98);
+jothamsCoolPlaylist.addTrack("Pingu In Da Club", 5, 127);
+jothamsCoolPlaylist.addTrack("One More Night", 5, 188);
+jothamsCoolPlaylist.addTrack("Call Me Maybe", 1, 244);
+var jothamsOtherPlaylist = jothamsLibrary.addPlaylist("Jotham's Second, Less Funky Playlist");
+jothamsOtherPlaylist.addTrack("Worst Song Ever", 1, 8);
+jothamsOtherPlaylist.addTrack("Coolest Song Ever", 3, 108);
+jothamsOtherPlaylist.addTrack("Africa", 5, 199);
+jothamsOtherPlaylist.addTrack("Not Dre Anymore", 4, 258);
+
+console.log("jothamsLibrary", jothamsLibrary);
+console.log("jothamsCoolPlaylist", jothamsCoolPlaylist);
+console.log("jothamsOtherPlaylist", jothamsOtherPlaylist);
+console.log("jothamsOtherPlaylist total duration", jothamsOtherPlaylist.totalDuration);
+console.log("jothamsOtherPlaylist over all", jothamsOtherPlaylist.overallRating);
